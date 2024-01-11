a8907031b793df0a4637d50a9b5a980a41f8a8d7
Victor-Senishyn <senishin2004@gmail.com>
2024-01-11 20:13:26 +0200

    Applied suggestions from code review

---
+++ CinemaSim/CinemaSim/AnimatedMovie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaSim
+{
+    public class AnimatedMovie : Movie
+    {
+        public string AnimationType { get; }
+
+        public AnimatedMovie(string name, ushort releaseDate, string timeOfTheFilm, string animationType) 
+            : base(name, releaseDate, timeOfTheFilm)
+            => AnimationType = animationType;
+    }
+}
---
+++ CinemaSim/CinemaSim/ForeignMovie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaSim
+{
+    public class ForeignMovie : Movie
+    {
+        public string Language;
+
+        public ForeignMovie(string name, ushort releaseDate, string timeOfTheFilm, string language) 
+            : base(name, releaseDate, timeOfTheFilm)
+            => Language = language;
+    }
+}
