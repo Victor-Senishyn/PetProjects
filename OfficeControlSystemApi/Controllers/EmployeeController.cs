d1af825eb75f7cfe49964482fc037a0df3fbd4f2
Victor-Senishyn <senishin2004@gmail.com>
2024-02-15 10:23:25 +0200

    Added new controllers

--- OfficeControlSystemApi/Controllers/EmployeeController.cs
+++ OfficeControlSystemApi/Controllers/EmployeeController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OfficeControlSystemApi.Data;
+using OfficeControlSystemApi.Models.DTOs;
+using OfficeControlSystemApi.Services;
 
 namespace OfficeControlSystemApi.Controllers
 {
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
+        private readonly EmployeeService _employeeService;
+        private readonly AccessCardService _accessCardService;
+        private readonly VisitHistoryService _visitHistoryService;
+
+        public EmployeeController(AppDbContext context)
         {
-            return View();
+            _employeeService = new EmployeeService(context);
+            _accessCardService = new AccessCardService(context);
+            _visitHistoryService = new VisitHistoryService(context);
+        }
+
+        [HttpPost("employee")]
+        public async Task<IActionResult> CreateEmployee([FromBody] EmployeeDto employeeInput, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var newEmployee = await _employeeService.CreateEmployeeAsync(employeeInput, cancellationToken);
+                var newAccessCard = await _accessCardService.CreateAccessCardAsync(employeeInput, cancellationToken);
+                var newVisitHistory = await _visitHistoryService.CreateVisitHistoryAsync(newAccessCard, cancellationToken);
+
+                return Ok(newEmployee);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex) when (ex is OperationCanceledException or TaskCanceledException)
+            {
+                return BadRequest("Request canceled due to user action or timeout.");
+            }
         }
     }
 }
--- OfficeControlSystemApi/Controllers/VisitHistoryController.cs
+++ OfficeControlSystemApi/Controllers/VisitHistoryController.cs
@@ -1,12 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OfficeControlSystemApi.Data;
+using OfficeControlSystemApi.Services;
 
 namespace OfficeControlSystemApi.Controllers
 {
     public class VisitHistoryController : Controller
     {
-        public IActionResult Index()
+        private readonly AccessCardService _accessCardService;
+        private readonly VisitHistoryService _visitHistoryService;
+
+        public VisitHistoryController(AppDbContext context)
         {
-            return View();
+            _accessCardService = new AccessCardService(context);
+            _visitHistoryService = new VisitHistoryService(context);
+        }
+
+        [HttpPut("exit/{visitHistoryId}")]
+        public async Task<IActionResult> UpdateExitDateTimeAsync(long visitHistoryId, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var newVisitHistory = await _visitHistoryService.UpdateExitDateTime(visitHistoryId, cancellationToken);
+                return Ok(newVisitHistory);
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex) when (ex is OperationCanceledException or TaskCanceledException)
+            {
+                return BadRequest("Request canceled due to user action or timeout.");
+            }
+        }
+
+        [HttpPost("visit/{accessCardId}")]
+        public async Task<IActionResult> AddVisitHistory(long accessCardId, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var newAccessCard = await _accessCardService.GetAccessCardByIdAsync(accessCardId, cancellationToken);
+                var newVisitHistory = await _visitHistoryService.CreateVisitHistoryAsync(newAccessCard, cancellationToken);
+
+                return Ok(newVisitHistory);
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex) when (ex is OperationCanceledException or TaskCanceledException)
+            {
+                return BadRequest("Request canceled due to user action or timeout.");
+            }
         }
     }
 }
