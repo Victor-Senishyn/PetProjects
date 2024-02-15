6e60d9b4e6b0b674550a9ab22c50f57d430c69e0
Victor-Senishyn <senishin2004@gmail.com>
2024-02-15 23:25:29 +0200

    Updated controllers

--- OfficeControlSystemApi/Controllers/EmployeeController.cs
+++ OfficeControlSystemApi/Controllers/EmployeeController.cs
@@ -12,30 +12,36 @@ namespace OfficeControlSystemApi.Controllers
         private readonly IAccessCardService _accessCardService;
         private readonly IVisitHistoryService _visitHistoryService;
         private readonly IScopedService _scoped;
+        private readonly AppDbContext _dbContext;
+
 
         public EmployeeController(
             IEmployeeService employeeService, 
             IAccessCardService accessCardService, 
             IVisitHistoryService visitHistoryService,
-            IScopedService scoped
+            IScopedService scoped,
+            AppDbContext dbContext
             )
         {
             _employeeService = employeeService;
             _accessCardService = accessCardService;
             _visitHistoryService = visitHistoryService;
-
             _scoped = scoped;
+            _dbContext = dbContext;
         }
 
         [HttpPost("employee")]
         public async Task<IActionResult> CreateEmployee([FromBody] EmployeeDto employeeInput, CancellationToken cancellationToken)
         {
+            using var transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
+
             try
             {
                 var newEmployee = await _employeeService.CreateEmployeeAsync(employeeInput, cancellationToken);
                 var newAccessCard = await _accessCardService.CreateAccessCardAsync(employeeInput, cancellationToken);
                 var newVisitHistory = await _visitHistoryService.CreateVisitHistoryAsync(newAccessCard, cancellationToken);
 
+                await transaction.CommitAsync(cancellationToken);
                 return Ok(newEmployee);
             }
             catch (ArgumentException ex)
@@ -44,6 +50,7 @@ namespace OfficeControlSystemApi.Controllers
             }
             catch (Exception ex) when (ex is OperationCanceledException or TaskCanceledException)
             {
+                await transaction.RollbackAsync(cancellationToken);
                 return BadRequest("Request canceled due to user action or timeout.");
             }
         }
